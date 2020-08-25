7ff07d0705fa04108a5b9f0c542ddd011fc0f273
irianjoubert1 <irian.joubert@gmail.com>
2020-08-25 11:22:22 -0300

    Configuring environment property

--- src/main/java/local/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/local/springframework/sfgdi/config/PropertyConfig.java
@@ -1,15 +1,21 @@
 package local.springframework.sfgdi.config;
 
 import local.springframework.sfgdi.exampleBean.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
     @Value("${local.user}")
     String user;
 
@@ -22,7 +28,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USER"));
         fakeDataSource.setPass(pass);
         fakeDataSource.setUrl(url);
 
