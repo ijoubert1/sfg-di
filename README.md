8f6b7b2704b35b9107e2cee6ed62020eb861ebaf
irianjoubert1 <irian.joubert@gmail.com>
2020-08-25 11:36:51 -0300

    Using application.properties

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # sfg-di
 Spring Framewrok Guru - Dependency Injection
-
-
-# Environment Properties Example (IntelliJ Configuration)
-Run/Debug Configurations -> Environment variables -> New -> USER=test
--- src/main/java/local/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/local/springframework/sfgdi/config/PropertyConfig.java
@@ -1,21 +1,12 @@
 package local.springframework.sfgdi.config;
 
 import local.springframework.sfgdi.exampleBean.FakeDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
-
     @Value("${local.user}")
     String user;
 
@@ -26,18 +17,12 @@ public class PropertyConfig {
     String url;
 
     @Bean
-    public FakeDataSource fakeDataSource(){
+    public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USER"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPass(pass);
         fakeDataSource.setUrl(url);
 
         return fakeDataSource;
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
     }
 }
